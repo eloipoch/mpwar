1969268b4957bb8e93b98a32aa7bafd94995064d
Eloi Poch <eloi.poch@gmail.com>
2015-03-02 00:15:42 +0100

    remove not required code & dependencies
     - Remove Acme Bundle & Code
     - Remove Nelmio Api Doc bundle
     - Remove Security Bundle
     - Remove Assetic Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,6 +1,5 @@
 <?php
 
-use Acme\DemoBundle\AcmeDemoBundle;
 use Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle;
 use Bazinga\Bundle\RestExtraBundle\BazingaRestExtraBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
@@ -8,17 +7,14 @@ use FOS\RestBundle\FOSRestBundle;
 use Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle;
 use JMS\SerializerBundle\JMSSerializerBundle;
 use MPWAR\Infrastructure\Symfony\Bundle\MPWARBundle;
-use Nelmio\ApiDocBundle\NelmioApiDocBundle;
 use Oracle\Infrastructure\Symfony\OracleBundle;
 use Sensio\Bundle\DistributionBundle\SensioDistributionBundle;
 use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
 use Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle;
 use SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle;
 use SimpleBus\SymfonyBridge\SimpleBusEventBusBundle;
-use Symfony\Bundle\AsseticBundle\AsseticBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
 use Symfony\Component\HttpKernel\Kernel;
@@ -31,15 +27,12 @@ class AppKernel extends Kernel
         $bundles = [
             new FrameworkBundle(),
             new SensioFrameworkExtraBundle(),
-            new SecurityBundle(),
             new TwigBundle(),
-            new AsseticBundle(),
 
             new MonologBundle(),
             new DoctrineBundle(),
 
             new FOSRestBundle(),
-            new NelmioApiDocBundle(),
             new JMSSerializerBundle(),
             new BazingaHateoasBundle(),
             new HautelookTemplatedUriBundle(),
@@ -52,7 +45,6 @@ class AppKernel extends Kernel
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
-            $bundles[] = new AcmeDemoBundle();
             $bundles[] = new WebProfilerBundle();
             $bundles[] = new SensioDistributionBundle();
             $bundles[] = new SensioGeneratorBundle();
